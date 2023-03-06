c2e5c89aeea0d2fc07afe3da455d7011213c3081
Micky_N <m.ndinga@speedmedia.fr>
2023-03-06 12:06:48 +0100

    editor class

--- src/components/Notes/Editor.vue
+++ src/components/Notes/Editor.vue
@@ -28,6 +28,76 @@ import Tooltip from 'editorjs-tooltip';
 import Underline from '@editorjs/underline';
 import { ref } from 'vue';
 
+class Separator {
+    /**
+     * Notify core that read-only mode is supported
+     * @return {boolean}
+     */
+    static get isReadOnlySupported() {
+        return true;
+    }
+
+    /**
+     * Allow Tool to have no content
+     * @return {boolean}
+     */
+    static get contentless() {
+        return true;
+    }
+
+    /**
+     * Render plugin`s main Element and fill it with saved data
+     *
+     * @param {{data: DelimiterData, config: object, api: object}}
+     *   data — previously saved data
+     *   config - user config for Tool
+     *   api - Editor.js API
+     */
+    constructor({ data, config, api }) {
+        this._CSS = 'separator';
+        this._element = this.drawView();
+    }
+
+    drawView() {
+        let div = document.createElement('div');
+        div.classList.add(this._CSS);
+        return div;
+    }
+
+    /**
+     * Return Tool's view
+     * @returns {HTMLDivElement}
+     * @public
+     */
+    render() {
+        return this._element;
+    }
+
+    /**
+     * Extract Tool's data from the view
+     * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
+     * @returns {DelimiterData} - saved data
+     * @public
+     */
+    save(toolsContent) {
+        return {};
+    }
+
+    /**
+     * Get Tool toolbox settings
+     * icon - Tool icon's SVG
+     * title - title to show in toolbox
+     *
+     * @return {{icon: string, title: string}}
+     */
+    static get toolbox() {
+        return {
+            title: 'Line',
+            icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>',
+        };
+    }
+}
+
 const editor = new EditorJS({
     onReady: () => {
         new DragDrop(editor);
@@ -133,6 +203,7 @@ const editor = new EditorJS({
             inlineToolbar: true,
             shortcut: 'CMD+ALT+T',
         },
+        separator: Separator,
     },
     data: {
         blocks: [
@@ -146,8 +217,13 @@ const editor = new EditorJS({
             {
                 type: 'paragraph',
                 data: {
+                    alignment: 'right',
                     text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.',
                 },
+            },
+            {
+                type: 'separator',
+                data: {},
             },
             {
                 type: 'header',
@@ -232,7 +308,15 @@ const save = () => {
         .save()
         .then((outputData) => {
             const edjsParser = require('editorjs-parser');
-            const parser = new edjsParser();
+            const test = {
+                paragraph: function (data, config) {
+                    return `<p class='${config.paragraph.pClass} ${data.alignment}'>${data.text}</p>`;
+                },
+                separator: function () {
+                    return "<div class='separator'></div>";
+                },
+            };
+            const parser = new edjsParser(undefined, test);
             html.value = parser.parse(outputData);
             console.log(html.value);
             console.log('Article data: ', outputData);
@@ -243,4 +327,13 @@ const save = () => {
 };
 </script>
 
-<style></style>
+<style>
+.paragraph.right {
+    text-align: right;
+}
+.separator {
+    height: 1px;
+    margin: 30px auto;
+    background: #c5c5c5;
+}
+</style>
